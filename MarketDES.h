db9a710c22c526dd93acd47ae94215fa1f4d6aa4
Jordan Higgins <jordandhiggins@gmail.com>
2019-04-08 20:41:04 -0600

    broke market sim into smaller routines

--- MarketDES.h
+++ MarketDES.h
@@ -24,6 +24,7 @@ private:
 public:
     vector<double> serviceTimes; // where I put my times
     void addEvent(EventStruct &event);
+    void onArrival(EventStruct event);
     void runSim();
 };
 
--- main.cpp
+++ main.cpp
@@ -11,34 +11,26 @@
 using namespace std;
 
 int main(int argc, char* argv[]) {
-
+    if (argc < 5) {
+        cout << "Not enough arguments...\n";
+        exit(1);
+    }
     double customerArrivalRate = atof(argv[1]);
     double maxServiceTime = atof(argv[2]);
     int randomNumSeed = atoi(argv[3]);
     string which = (argv[4]);
-
     //    double avgCustomerArrivalRate = 0.5;
     // double maxServiceTime = 15.2;
     // int randomNumSeed = 34543;
-
     double totalMinutes = 12.0 * 60.0;
     int numCustomers = int (totalMinutes * customerArrivalRate);
     double totalSeconds = totalMinutes * 60;
     double maxServiceTimeSeconds = maxServiceTime * 60;
-
-    BankDES BankSim;
-    EventStruct myEvent;
     srand(randomNumSeed);
-
+    EventStruct myEvent;
     if(which == "-b"){
-
-        // cout << "*****************************" << endl;
-        // cout << "!!! Bank Queue Simulation !!!" << endl;
-        // cout << "*****************************\n" << endl;
-
-        
-
-    //    cout << "Adding " << numCustomers << " customers.\n";
+        BankDES BankSim;
+        //    cout << "Adding " << numCustomers << " customers.\n";
         for (int i = 0; i < numCustomers; i++) {
             myEvent.eventTime = fmod(rand(), totalSeconds);
             myEvent.duration = fmod(rand(), maxServiceTimeSeconds);
@@ -46,11 +38,9 @@ int main(int argc, char* argv[]) {
             myEvent.event = ARRIVAL;
             BankSim.addEvent(myEvent);
         }
-
         // cout << "Running...\n" << endl;
         BankSim.runSim();
         vector<double> bankTimes = BankSim.getServiceTimes();
-
         // cout << "Bank Service Times in Minutes: \n";
         // double averageTime = 0;
         // for(double d : bankTimes)
@@ -60,18 +50,9 @@ int main(int argc, char* argv[]) {
         // cout << "50th %ile " << bankTimes[(numCustomers * 0.5)] << endl;
         // cout << "90th %ile " << bankTimes[(numCustomers * 0.9)] << endl;
         // cout << "Average Service Time : " << averageTime/60 << " minutes" << endl;
-
-
         cout << bankTimes[(numCustomers * 0.9)] << endl;
-
-
     } else if (which == "-m"){
-        // cout << "************************************" << endl;
-        // cout << "!!! Supermarket Queue Simulation !!!" << endl;
-        // cout << "************************************\n" << endl;
-
         MarketDES MarketSim;
-
         for (int i = 0; i < numCustomers; i++) {
             myEvent.eventTime = fmod(rand(), totalSeconds);
             myEvent.duration = fmod(rand(), maxServiceTimeSeconds);
@@ -79,10 +60,8 @@ int main(int argc, char* argv[]) {
             myEvent.event = ARRIVAL;
             MarketSim.addEvent(myEvent);
         }
-
         // cout << "Running...\n" << endl;
         MarketSim.runSim();
-
         // cout << "\nSupermarket Service Times in Minutes: \n";
         // double averageTime = 0;
         // for(double d : MarketSim.serviceTimes)
@@ -93,9 +72,7 @@ int main(int argc, char* argv[]) {
         // cout << "50th %ile " << MarketSim.serviceTimes[(numCustomers * 0.5)] / 60 << endl;
         // cout << "90th %ile " << MarketSim.serviceTimes[(numCustomers * 0.9)] / 60 << endl;
         // cout << "Average Service Time : " << averageTime/60 << " minutes" << endl;
-
         cout << MarketSim.serviceTimes[(numCustomers * 0.9)] << endl;
-
     } else {
         cout << "E R R O R  F O O L" << endl;
         return 666;
